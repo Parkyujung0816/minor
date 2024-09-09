366bf1be1d474b59569ec5d8f2f45f50dc9ff7a8
Parkyujung0816 <pyjeronica@naver.com>
2024-09-09 16:18:50 +0900

    Add files via upload

---
+++ abc.R
@@ -0,0 +1,26 @@
+a=1
+a=10
+a=1
+b=3
+a+b
+print("a+b")
+print("hello")
+9%%2
+9%/%2
+9!=2
+9!=2
+9==2
+1 | b
+1 | a
+3 & a
+!(3==4)
+3!=4
+(5==6)|(8>=7)
+(5==6)%(8==7)
+(5==6)&(8==7)
+integer_2
+integer_var <-5
+numeric_var <- 3.14
+complex_var <-2+3i
+character_var <- "hello,wolrd!"
+logical_var <-TRUE
